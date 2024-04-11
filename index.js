6aacc486a5a9256a39033ff55d2e8192549dcf62
rohit <rp2012946@gmail.com>
2024-04-11 12:40:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js')
+
+const app = express()
+
+// middle ware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+
+app.get('/', (req, res) => {
+    res.send("hello from node api server");
+
+});
+
+
+// update a product 
+
+
+// delete a Product
+
+
+
+mongoose.connect("mongodb+srv://admin:qfTiSwCEpOAAyLME@backenddb.xybwmgp.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+    .then(() => {
+        console.log("Connected to database");
+        app.listen(3000, () => {
+            console.log("Server is running in 3000");
+        });
+    })
+    .catch(() => {
+        console.log("connection failed!");
+    })
---
+++ models/product.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: [true, "Please Enter Product Name"]
+        },
+
+        quantity: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+
+        price: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+
+        image: {
+            type: String,
+            require: false
+        },
+
+    },
+    {
+        timestamps: true,
+    }
+
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
